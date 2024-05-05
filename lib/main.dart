aabec71f77fa79c604190f83b9688fad9316fe0a
Junior da Silva Pereira <junior_silvapereira@hotmail.com>
2024-05-04 22:15:29 -0300

    categories meals screen

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'DeliMeals',
       theme: ThemeData(
-          primarySwatch: Colors.pink,
+          primaryColor: Colors.pink,
           fontFamily: 'Raleway',
           canvasColor: const Color.fromRGBO(255, 254, 229, 1),
           textTheme: ThemeData.light().textTheme.copyWith(
